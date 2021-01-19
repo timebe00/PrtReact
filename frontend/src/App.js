2cb5306b06a5968de43365d953e1baceb6331a39
timebe00 <an0160@naver.com>
2021-01-19 09:08:44 +0900

    arr control

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 // import Hellow from './component/Hellow'
 import './App.css'
 // import Counter from './component/Counter'
@@ -15,6 +15,35 @@ function App() {
   //   fontSize: 24,
   //   padding : '1rem'
   // }
+
+  //  users정보
+  const users = [
+    {
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+    }
+  ];
+
+  // 추가 시 4번째 부터 시작이라고 돔에 추가
+  const nextId = useRef(4);
+  //  추가시 호출 할 함수
+  const onCreate = () => {
+
+    //  추가 이후 5번째로 추가
+    nextId.current += 1
+  }
+
   return (
     <div>
       {/*<Wrapper> 넣기*/}
@@ -27,7 +56,8 @@ function App() {
       {/* <div className="gray-box"></div> */}
       {/* <Counter/> */}
       {/* <InputSample/> */}
-      <UserList/>
+      {/*users 정보를 parpam로 넘기기*/}
+      <UserList users = {users}/>
     </div>
   );
 }
